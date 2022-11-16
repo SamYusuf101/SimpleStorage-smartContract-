535fb67229c3f92699d86d1a2a8d1bea8900b519
SamYusuf101 <samyusuf101@gmail.com>
2022-11-16 02:44:59 -0800

    created add people function

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -2,7 +2,14 @@
 pragma solidity 0.8.8; // solidity version
 
 contract SimpleStorage {
-    uint256 public MyNumber;
+    uint256  MyNumber;
+
+    People[] public people;
+
+    struct People{
+        uint256 MyNumber;
+        string name;
+    }
 
     function store(uint _MyNumber) public {
         MyNumber=_MyNumber;
@@ -10,6 +17,11 @@ contract SimpleStorage {
 
     function retrieve() public view returns(uint256) {
         return MyNumber;
+    }
+
+    function addPeople (string memory _name, uint256 _MyNumber) public {
+          People memory newPeople= People({MyNumber:_MyNumber, name:_name});
+          people.push(newPeople);
     }
     
 }
